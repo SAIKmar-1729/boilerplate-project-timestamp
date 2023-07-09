b9bfab333f1863a5688716c3509a718ff248dcdb
SAIKmar-1729 <83260137+SAIKmar-1729@users.noreply.github.com>
2023-07-09 23:39:30 +0530

    Timestamp problem

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +21,38 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/:date?', (req, res) => {
+  const { date } = req.params;
+
+  if (!date) {
+    // If date parameter is empty, return the current time
+    const currentTime = new Date();
+    return res.json({ unix: currentTime.getTime(), utc: currentTime.toUTCString() });
+  }
+
+  const parsedDate = new Date(date);
+
+  if (isNaN(parsedDate)) {
+    // If the parsed date is invalid, check if it's a Unix timestamp
+    const timestamp = parseInt(date);
+    if (!isNaN(timestamp)) {
+      const unixDate = new Date(timestamp);
+      if (!isNaN(unixDate)) {
+        return res.json({ unix: unixDate.getTime(), utc: unixDate.toUTCString() });
+      }
+    }
+    // If not a valid date or Unix timestamp, return an error
+    return res.json({ error: 'Invalid Date' });
+  }
+
+  res.json({
+    unix: parsedDate.getTime(),
+    utc: parsedDate.toUTCString()
+  });
+});
 
 
 // listen for requests :)
